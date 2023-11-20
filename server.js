2c8e0f21168f811f139bd533250319d49cdd1a1d
Nicholas Botorog <nicholasbotorog@gmail.com>
2023-11-19 22:44:06 -0500

    GET Restaurant Route

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+const { Restaurant }  = require('./models/Restaurant')
+
 const app = require("./src/app")
 const db = require("./db/connection");
 const port = 3000;
 
 //TODO: Create your GET Request Route Below: 
+
+app.use('/restaurants', async(req, res) => { 
+    const restaurantList = await Restaurant.findAll()
+    console.log(restaurantList)
+    res.json(restaurantList)
+})
 
 
 app.listen(port, () => {
